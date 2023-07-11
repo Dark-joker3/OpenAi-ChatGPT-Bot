afb7411ede02be66511bcc3f4b6686bb9587f787
Black-KDX <121096454+Dark-joker3@users.noreply.github.com>
2023-07-11 16:46:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = ''
-global.SESSION_ID = ''
+global.SESSION_ID = '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'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
